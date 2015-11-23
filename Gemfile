86e50cb6dce6c9f34978ae0f3d87cc98e2fca1a7
Tijmen Brommet <tijmen@gmail.com>
2015-11-23 10:26:20 +0000

    Add capybara and webmock
    These are used for testing.

--- Gemfile
+++ Gemfile
@@ -26,3 +26,8 @@ end
 group :development do
   gem 'web-console', '~> 2.0'
 end
+
+group :test do
+  gem 'webmock', '~> 1.22'
+  gem 'capybara'
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,12 +6,15 @@ if ENV['USE_COVERAGE']
 end
 
 ENV['RAILS_ENV'] = 'test'
+ENV['GOVUK_APP_DOMAIN'] = 'test.gov.uk'
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'spec_helper'
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+require 'capybara/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
