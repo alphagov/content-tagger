426d4b950799e20c60efd3d6a9cad0b522abddf8
Tijmen Brommet <tijmen@gmail.com>
2015-11-23 10:26:20 +0000

    Add module to show title and breadcrumb
    This module is taken from collections-publisher. It might be extracted
    to govuk_admin_template later.

---
+++ app/helpers/header_helper.rb
@@ -0,0 +1,29 @@
+module HeaderHelper
+  # Generates a header with `title` and `breadcrumbs`. Last item in the
+  # breadcrumbs array should be a string for the "active" entry.
+  def display_header(title:, breadcrumbs:, page_title: nil)
+    breadcrumbs = breadcrumbs.compact
+    active_item = breadcrumbs.pop
+
+    locals = {
+      title: title,
+      breadcrumbs: breadcrumbs,
+      page_title: page_title || title,
+      active_item: active_item.try(:title) || active_item
+    }
+
+    render layout: 'shared/header', locals: locals do
+      yield if block_given?
+    end
+  end
+
+  def auto_link(object)
+    if object.is_a?(ActiveRecord::Base)
+      link_to object.title, object
+    elsif object.to_s.starts_with?('<a href')
+      raw object
+    else
+      link_to object.to_s.humanize, object.to_sym
+    end
+  end
+end
