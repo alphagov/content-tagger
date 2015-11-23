1b06f5086792e0ab11c0da6efe4f9c21cbdeeb22
Tijmen Brommet <tijmen@gmail.com>
2015-11-23 11:03:02 +0000

    Reset database before test run
    This creates the database, which is used for SSO.

--- jenkins.sh
+++ jenkins.sh
@@ -38,8 +38,7 @@ git merge --no-commit origin/master || git merge --abort
 export RAILS_ENV=test
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment --without development
 
-# Uncomment if this app uses a database
-# bundle exec rake db:drop db:create db:schema:load
+bundle exec rake db:drop db:create db:schema:load
 
 if bundle exec rake ${TEST_TASK:-"default"}; then
   github_status success "succeeded on Jenkins"
