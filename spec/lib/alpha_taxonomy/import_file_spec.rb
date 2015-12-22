3ca0bec09e60fc880e3515422514e437dcba566d
Mo Baig <mo@mobaig.com>
2015-12-22 16:56:01 +0000

    DRY up ImportFile spec
    Reduce some of the overhead of stubbing out test data from the SheetDownloader
    class, simplify expectations about the compiled tsv file content.

--- spec/lib/alpha_taxonomy/import_file_spec.rb
+++ spec/lib/alpha_taxonomy/import_file_spec.rb
@@ -17,26 +17,44 @@ RSpec.describe AlphaTaxonomy::ImportFile do
     after do
       File.delete(test_tsv_file_path) if File.exist?(test_tsv_file_path)
     end
+
+    def stub_sheet_data(tsv_rows)
+      sheet_data = (["mapped to\tlink"] + tsv_rows).join("\n") + "\n"
+      allow(sheet_downloader).to receive(:each_sheet).and_yield(sheet_data)
+    end
+
+    def expected_tsv_content(tsv_rows)
+      (["taxon_title\tbase_path"] + tsv_rows).join("\n") + "\n"
     end
 
     it "parses and writes the required data to a file" do
-      test_tsv_data = [
-        "mapped to\t" + "link",
-        "Foo-Taxon\t" + "the-foo-link",
-        "Bar (Br)| Baz (Bz)\t" + "the-bar-or-baz-link",
-        "n/a - not applicable\t" + "the-n/a-link",
-      ].join("\n")
-      allow(sheet_downloader).to receive(:each_sheet).and_yield(test_tsv_data)
+      stub_sheet_data([
+        "Foo-Taxon\t" + "/foo-content-item-path",
+        "Bar (Br)| Baz (Bz)\t" + "/bar-or-baz-content-item-path",
+        "n/a - not applicable\t" + "/n/a-content-item-path",
+      ])
 
       AlphaTaxonomy::ImportFile.new.populate
 
       populated_file = File.open(test_tsv_file_path)
-      expect(populated_file.read.split("\n")).to eq([
-        "taxon_title\tlink",
-        "Foo-Taxon\tthe-foo-link",
-        "Bar (Br)\tthe-bar-or-baz-link",
-        "Baz (Bz)\tthe-bar-or-baz-link"
-      ])
+      expect(populated_file.read).to eq(
+        expected_tsv_content([
+          "Foo-Taxon\t/foo-content-item-path",
+          "Bar (Br)\t/bar-or-baz-content-item-path",
+          "Baz (Bz)\t/bar-or-baz-content-item-path"
+        ])
+      )
+    end
+
+    it "reports an error and removes the file if the required values aren't present" do
+      stub_sheet_data([ "\t" + "the-foo-slug", ])
+
+      log_output = StringIO.new
+      AlphaTaxonomy::ImportFile.new(logger: Logger.new(log_output)).populate
+
+      log_output.rewind
+      expect(log_output.read).to match(/Missing value in downloaded taxonomy spreadsheet/)
+      expect(File.exist?(test_tsv_file_path)).to be false
     end
 
     it "reports an error and removes the file if the expected columns aren't present" do
@@ -51,21 +69,6 @@ RSpec.describe AlphaTaxonomy::ImportFile do
 
       log_output.rewind
       expect(log_output.read).to match(/Column names in downloaded taxonomy data did not match expected values/)
-      expect(File.exist?(test_tsv_file_path)).to be false
-    end
-
-    it "reports an error and removes the file if the required values aren't present" do
-      test_tsv_data = [
-        "mapped to\t" + "link",
-        "\t" + "the-foo-slug",
-      ].join("\n")
-      allow(sheet_downloader).to receive(:each_sheet).and_yield(test_tsv_data)
-
-      log_output = StringIO.new
-      AlphaTaxonomy::ImportFile.new(logger: Logger.new(log_output)).populate
-
-      log_output.rewind
-      expect(log_output.read).to match(/Missing value in downloaded taxonomy spreadsheet/)
       expect(File.exist?(test_tsv_file_path)).to be false
     end
   end
