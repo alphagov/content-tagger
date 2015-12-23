ae413843722a9a8e307e32bf955b34cb7feec1e5
Mo Baig <mo@mobaig.com>
2015-12-23 12:18:09 +0000

    Add method to ImportFile which returns mappings grouped by base path
    This is required as part of the functionality to bulk assign content
    items to taxons, based on the alpha taxonomy data that is currently
    being produced by Information Architects in the Finding Things team.
    
    This change adds a method which inspects the locally-downloaded taxonomy
    data and groups list of taxons to each base path. Part of this change
    includes moving the actual file creation to the #populate method rather
    than doing this on initialize. Any client of the ImportFile class will
    have to ensure the file is created via #populate before calling anything
    else.

--- spec/lib/alpha_taxonomy/import_file_spec.rb
+++ spec/lib/alpha_taxonomy/import_file_spec.rb
@@ -1,34 +1,36 @@
 require "rails_helper"
 
 RSpec.describe AlphaTaxonomy::ImportFile do
+  let(:test_tsv_file_path) do
+    FileUtils.mkdir_p Rails.root + "tmp"
+    Rails.root + "tmp/import_file_spec.csv"
+  end
+  let(:sheet_downloader) { AlphaTaxonomy::SheetDownloader.new }
+
+  before do
+    allow(AlphaTaxonomy::ImportFile).to receive(:location).and_return(test_tsv_file_path)
+    allow(AlphaTaxonomy::SheetDownloader).to receive(:new).and_return(sheet_downloader)
+  end
+
+  after do
+    File.delete(test_tsv_file_path) if File.exist?(test_tsv_file_path)
+  end
+
+  def stub_downloaded_sheet_data(tsv_rows)
+    # Create a string of tsv data from an array of rows, resulting in
+    # tab-seperated values with newlines delimiting each row.
+    headers = ["mapped to\tlink"]
+    sheet_data = (headers + tsv_rows).join("\n") + "\n"
+    allow(sheet_downloader).to receive(:each_sheet).and_yield(sheet_data)
+  end
+
   describe "#populate" do
-    let(:test_output_path) do
-      FileUtils.mkdir_p Rails.root + "tmp"
-      Rails.root + "tmp/import_file_spec.csv"
-    end
-
-    let(:sheet_downloader) { AlphaTaxonomy::SheetDownloader.new }
-
-    before do
-      allow(AlphaTaxonomy::ImportFile).to receive(:location).and_return(test_tsv_file_path)
-      allow(AlphaTaxonomy::SheetDownloader).to receive(:new).and_return(sheet_downloader)
-    end
-
-    after do
-      File.delete(test_tsv_file_path) if File.exist?(test_tsv_file_path)
-    end
-
-    def stub_sheet_data(tsv_rows)
-      sheet_data = (["mapped to\tlink"] + tsv_rows).join("\n") + "\n"
-      allow(sheet_downloader).to receive(:each_sheet).and_yield(sheet_data)
-    end
-
     def expected_tsv_content(tsv_rows)
       (["taxon_title\tbase_path"] + tsv_rows).join("\n") + "\n"
     end
 
     it "parses and writes the required data to a file" do
-      stub_sheet_data([
+      stub_downloaded_sheet_data([
         "Foo-Taxon\t" + "/foo-content-item-path",
         "Bar (Br)| Baz (Bz)\t" + "/bar-or-baz-content-item-path",
         "n/a - not applicable\t" + "/n/a-content-item-path",
@@ -47,7 +49,7 @@ RSpec.describe AlphaTaxonomy::ImportFile do
     end
 
     it "reports an error and removes the file if the required values aren't present" do
-      stub_sheet_data([ "\t" + "the-foo-slug", ])
+      stub_downloaded_sheet_data(["\t" + "the-foo-slug"])
 
       log_output = StringIO.new
       AlphaTaxonomy::ImportFile.new(logger: Logger.new(log_output)).populate
@@ -70,6 +72,37 @@ RSpec.describe AlphaTaxonomy::ImportFile do
       log_output.rewind
       expect(log_output.read).to match(/Column names in downloaded taxonomy data did not match expected values/)
       expect(File.exist?(test_tsv_file_path)).to be false
+    end
+  end
+
+  describe "#grouped_mappings" do
+    context "if the import file is present" do
+      before do
+        stub_downloaded_sheet_data([
+          "Foo-Taxon\t" + "/foo-content-item-path",
+          "Bar (Br)| Baz (Bz)\t" + "/bar-or-baz-content-item-path",
+        ])
+        AlphaTaxonomy::ImportFile.new.populate
+      end
+
+      it "returns lists of taxons grouped by base_path" do
+        expect(AlphaTaxonomy::ImportFile.new.grouped_mappings).to eq(
+          {
+            "/foo-content-item-path" => ["Foo-Taxon"],
+            "/bar-or-baz-content-item-path"=> ["Bar (Br)", "Baz (Bz)"]
+          }
+        )
+      end
+    end
+
+    context "if the import file is missing" do
+      it "raises an error" do
+        allow(AlphaTaxonomy::ImportFile).to receive(:location).and_return("/some-crazy-non-existing-path")
+
+        expect { AlphaTaxonomy::ImportFile.new.grouped_mappings }.to raise_error(
+          AlphaTaxonomy::ImportFile::MissingImportFileError
+        )
+      end
     end
   end
 end
