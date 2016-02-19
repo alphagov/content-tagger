3e4594a83e5d20def0ce2f8692616bd26bc7da0f
Mo Baig <mo@mobaig.com>
2016-02-19 14:01:32 +0000

    Taxon bulk importer - add comment to method in ImportFile regarding return value

--- lib/alpha_taxonomy/import_file.rb
+++ lib/alpha_taxonomy/import_file.rb
@@ -36,6 +36,11 @@ module AlphaTaxonomy
       File.delete(@file.path) if File.exist?(@file.path)
     end
 
+    # Return a hash in the following form
+    # {
+    #   '/content-base-path-1' => [ 'taxon-title-1', 'taxon-title-2' ],
+    #   '/content-base-path-2' => [ 'taxon-title-2', 'taxon-title-3' ],
+    # }
     def grouped_mappings
       check_import_file_is_present
       mappings = CSV.read(self.class.location, col_sep: "\t", headers: true)
