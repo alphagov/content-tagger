e578129d37a890d926ee97b252dd25679f5ff11d
Carlos Vilhena <carlosvilhena@gmail.com>
2016-07-20 13:23:02 +0000

    Bring back necessary code to run taxon functionality
    We copied code from collections-publisher regarding taxons. In doing so, we also
    kept the git history. As that would bring way too many commits to this repo
    (especially unrelated ones), we removed common code from that merge (e.g. code
    that touches application_controller.rb, etc). That keeps our history nice and
    short, however we still need to bring back the glue code that makes this feature
    work.
    
    This commit does exactly that.

--- lib/services.rb
+++ lib/services.rb
@@ -1,4 +1,5 @@
 require 'gds_api/publishing_api_v2'
+require 'gds_api/content_store'
 
 module Services
   def self.publishing_api
@@ -6,6 +7,13 @@ module Services
       Plek.new.find('publishing-api'),
       disable_cache: true,
       bearer_token: ENV['PUBLISHING_API_BEARER_TOKEN'] || 'example',
+    )
+  end
+
+  def self.content_store
+    @content_store ||= GdsApi::ContentStore.new(
+      Plek.new.find('content-store'),
+      disable_cache: true
     )
   end
 end
