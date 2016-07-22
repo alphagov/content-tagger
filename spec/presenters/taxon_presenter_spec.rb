4ab9813efd3f70fb65d24410b8ad868bc30105d4
Carlos Vilhena <carlosvilhena@gmail.com>
2016-07-22 14:08:41 +0000

    Fix `publishing_app` name on the Taxon Presenter
    This code was originally copied from `collections-publisher`. Back then, the
    `publishing_app` was correct, but not anymore. This commit fixes it.

--- spec/presenters/taxon_presenter_spec.rb
+++ spec/presenters/taxon_presenter_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 RSpec.describe TaxonPresenter do
   describe "#payload" do
-    it "generates a valid payload" do
-      presenter = TaxonPresenter.new(
+    let(:presenter) do
+      TaxonPresenter.new(
         title: "My Title",
         base_path: "/taxons/my-taxon"
       )
+    end
+    let(:payload) { presenter.payload }
 
-      payload = presenter.payload
-
+    it "generates a valid payload" do
       expect(payload).to be_valid_against_schema('taxon')
+    end
+
+    it 'assigns the expected rendering app' do
+      expect(payload[:publishing_app]).to eq('content-tagger')
     end
   end
 end
