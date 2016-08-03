c3537e993b878417dc5fa44fb4b3047ffc112a76
Mo Baig <mo@mobaig.com>
2016-08-03 16:27:14 +0100

    Refactor tag import progress bar javascript
    Use GOV.UK modules framework, to bring the structure in line with some
    of the other publishing apps.

--- app/assets/javascripts/import-progress.js
+++ app/assets/javascripts/import-progress.js
@@ -1,23 +1,31 @@
-$(function updateImportProgress() {
-  var $importProgress = $('.js-import-progress');
-  if ($importProgress.length == 0) { return; }
+(function(Modules) {
+  "use strict";
 
-  var currentProgress = $importProgress.find('.progress-bar').attr("aria-valuenow");
-  var maxProgress = $importProgress.find('.progress-bar').attr("aria-valuemax");
+  Modules.ImportProgress = function() {
+    var that = this;
 
-  // Don't poll for progress if the counter sits at zero - means you have to
-  // refresh the page a couple of times to see progress but avoids pointless
-  // network requests in cases where an import hasn't begun yet.
-  if (currentProgress == 0) { return; }
+    that.start = function(element) {
+      updateImportProgress();
 
-  if (maxProgress - currentProgress != 0) {
-    var progressPath = $importProgress.data('progress-path');
-    var updatedProgress = $.get(progressPath).done(
-      function(data) {
-        $importProgress.replaceWith(data);
-        setTimeout(updateImportProgress, 2000);
+      function updateImportProgress() {
+        var currentProgress = element.find('.progress-bar').attr("aria-valuenow");
+        var maxProgress = element.find('.progress-bar').attr("aria-valuemax");
+
+        // Don't poll for progress if the counter sits at zero - means you have to
+        // refresh the page a couple of times to see progress but avoids pointless
+        // network requests in cases where an import hasn't begun yet.
+        if (currentProgress == 0) { return; }
+
+        if (maxProgress - currentProgress != 0) {
+          var progressPath = element.find('.js-import-progress').data('progress-path');
+          var updatedProgress = $.get(progressPath).done(
+            function(data) {
+              element.find('.js-import-progress').replaceWith(data);
+              setTimeout(updateImportProgress, 2000);
+            }
+          );
+        }
       }
-    );
-  }
-});
-
+    }
+  };
+})(window.GOVUKAdmin.Modules);
