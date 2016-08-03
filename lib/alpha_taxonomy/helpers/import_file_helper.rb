bc75120009a9b17e385da5d81eeba6acbdb5eba0
Mo Baig <mo@mobaig.com>
2016-08-03 20:44:12 +0100

    Remove AlphaTaxonomy bulk tagging scripts
    The Tag Importer UI now makes these rake tasks and scripts largely
    redundant - they're fiddly to run and offer no particular advantage
    over the browser-based option.

--- lib/alpha_taxonomy/helpers/import_file_helper.rb
+++
@@ -1,9 +0,0 @@
-module AlphaTaxonomy
-  module Helpers
-    module ImportFileHelper
-      def check_import_file_is_present
-        raise AlphaTaxonomy::SharedExceptions::MissingImportFileError unless File.exist? AlphaTaxonomy::ImportFile.location
-      end
-    end
-  end
-end
