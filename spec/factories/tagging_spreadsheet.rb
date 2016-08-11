a49f95e1c0d60dbf8a2bf3ac796949ff5df163df
Tijmen Brommet <tijmen@gmail.com>
2016-08-11 17:16:33 +0100

    Make sure we don't put invalid data into tagmappings table
    This adds NOT NULL constraints to a bunch of tables that should never
    have null-values. Makes it easier to spot mistakes.

--- spec/factories/tagging_spreadsheet.rb
+++ spec/factories/tagging_spreadsheet.rb
@@ -5,7 +5,7 @@ include GoogleSheetHelper
 FactoryGirl.define do
   factory :tagging_spreadsheet do
     url google_sheet_url(key: 'mykey', gid: 'mygid')
-
+    user_uid { SecureRandom.uuid }
     state 'uploaded'
   end
 end
