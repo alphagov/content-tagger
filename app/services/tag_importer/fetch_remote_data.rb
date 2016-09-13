758aaf184e3727e80bb7aa87465510b39fcacb25
Mo Baig <mo@mobaig.com>
2016-09-13 14:14:28 +0100

    Handle blank values when processing remote TSV data
    We have not null DB constraints on several fields in the TagMapping
    model. An import TSV may have blank values in any of its fields. We
    should handle these cases and not attempt to persist null values in the
    tag_mappings table.

--- app/services/tag_importer/fetch_remote_data.rb
+++ app/services/tag_importer/fetch_remote_data.rb
@@ -32,10 +32,10 @@ module TagImporter
 
     def save_row(row)
       tagging_spreadsheet.tag_mappings.build(
-        content_base_path:  row["content_base_path"],
+        content_base_path:  String(row["content_base_path"]),
         link_title:         row["link_title"],
-        link_content_id:    row["link_content_id"],
-        link_type:          row["link_type"],
+        link_content_id:    String(row["link_content_id"]),
+        link_type:          String(row["link_type"]),
         state:              'ready_to_tag'
       ).save
     end
