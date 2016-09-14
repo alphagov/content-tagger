67dd4584ebabc92a392dcf3b1b643ff2615a60d8
Tijmen Brommet <tijmen@gmail.com>
2016-09-14 11:49:19 +0100

    Fix migration for polymorphic tag mappings
    The `taggable_type` attribute got renamed from `taggable_type`.
    
    Because locally there weren't any records in the tag_mappings table,
    this didn't surface. On staging there are records, so this migration
    fails because `taggable_type` doesn't exist.
    
    Also makes it use a SQL query, which is faster and more robust.

--- db/migrate/20160902132101_make_tag_mapping_polymorphic.rb
+++ db/migrate/20160902132101_make_tag_mapping_polymorphic.rb
@@ -3,10 +3,6 @@ class MakeTagMappingPolymorphic < ActiveRecord::Migration
     rename_column :tag_mappings, :tagging_spreadsheet_id, :tagging_source_id
     add_column :tag_mappings, :tagging_source_type, :string
 
-    TagMapping.transaction do
-      TagMapping.all.each do |tag_mapping|
-        tag_mapping.update!(taggable_type: 'TaggingSpreadsheet')
-      end
-    end
+    execute "UPDATE tag_mappings SET tagging_source_type = 'TaggingSpreadsheet'"
   end
 end
