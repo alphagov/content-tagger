be15dc967226f33c1d75b5ec72491f1fba7c0f2f
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-09-15 12:35:18 +0000

    Add aggregated_tag_mappings to Spreadsheet model

--- app/models/tagging_spreadsheet.rb
+++ app/models/tagging_spreadsheet.rb
@@ -20,4 +20,28 @@ class TaggingSpreadsheet < ActiveRecord::Base
   def error_count
     tag_mappings.errored.count
   end
+
+  def aggregated_tag_mappings
+    aggregated_tag_mappings = []
+
+    tag_mappings_grouped_by_content_base_path.reduce([]) do |acc, aggregation|
+      aggregated_tag_mappings << AggregatedTagMapping.new(content_base_path: aggregation.first, tag_mappings: aggregation.last)
+    end
+
+    aggregated_tag_mappings
+  end
+
+private
+  def tag_mappings_grouped_by_content_base_path
+    tag_mappings.by_state.by_content_base_path.by_link_title.
+      select(
+        :id,
+        :link_type,
+        :link_title,
+        :content_base_path,
+        :message,
+        :link_content_id,
+        :state).
+      group_by(&:content_base_path)
+  end
 end
