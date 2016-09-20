cf54ff204acfaf6d1c9cb091e9fbf55962be0c7e
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-09-20 15:12:37 +0100

    Show documents tagged to a taxon

--- app/services/bulk_tagging/fetch_tagged_content.rb
+++ app/services/bulk_tagging/fetch_tagged_content.rb
@@ -12,6 +12,30 @@ module BulkTagging
     end
 
     def call
+      if taxon_document_type?
+        request_tagged_content_to_taxon
+      else
+        request_tagged_content_items
+      end
+    end
+
+  private
+
+    def taxon_document_type?
+      document_type == "taxon"
+    end
+
+    def request_tagged_content_to_taxon
+      results = Services.publishing_api.get_linked_items(
+        content_id,
+        link_type: "taxons",
+        fields: %w(title content_id base_path document_type)
+      )
+
+      results.map { |result| ContentItem.new(result) }
+    end
+
+    def request_tagged_content_items
       api_response = Services.publishing_api.get_expanded_links(content_id)
       results = api_response['expanded_links'].fetch(
         BulkTaggingSource.new.content_key_for(document_type), []
