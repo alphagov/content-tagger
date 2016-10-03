5b3160f7d5aa610abc96efddde394e0f72436d04
Carlos Vilhena <carlosvilhena@gmail.com>
2016-10-03 09:43:42 +0000

    Revert "Add rake task to clear parent links for all taxons"
    This reverts commit a817d621b7f29c3095b2b4681e6fc4a65f0b77cb.

--- app/services/taxonomy/clear_parent_link.rb
+++
@@ -1,9 +0,0 @@
-module Taxonomy
-  class ClearParentLink
-    def self.call(content_id)
-      taxon_parent_links_update = TaxonParentLinksUpdate.new(content_id)
-
-      PublishLinks.call(links_update: taxon_parent_links_update)
-    end
-  end
-end
