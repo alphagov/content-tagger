666ae9e4c15473e4e8164dcc564433b086a78574
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2016-12-07 14:54:40 +0000

    Add form for ordered related items link type
    Ordered related links can be any item on GOV.UK, so we capture base paths, and
    convert values to content ids when submitting them back to the publishing api.
    
    Accept both full URLs like "https://www.gov.uk/pay-vat" and relative paths
    like "/pay-vat" in the related content items section.
    
    This is more flexible, because users may want to enter either type of URL,
    and is consistent with the form for selecting a page to tag.
    
    The order that the user enters the related content MUST be the same as the order
    sent to the content store, because this is the order in which the related content
    will be displayed in the frontend
    
    If any of the related content item paths are invalid (i.e. do not correspond to a
    content ID in the content store), highlight that field and show an error message
    on the field, and add a Flash message to the top of the page.

--- spec/forms/tagging_update_form_spec.rb
+++ spec/forms/tagging_update_form_spec.rb
@@ -1,6 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe TaggingUpdateForm do
+  describe '#valid?' do
+    it "is valid if content item has no links" do
+      form = TaggingUpdateForm.new
+
+      expect(form).to be_valid
+    end
+
+    it "is valid if related item paths exist" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: ["/bank-holidays", "/pay-vat"],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/bank-holidays", "/pay-vat"] })
+        .to_return(body: {
+          "/bank-holidays" => "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "/pay-vat" => "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+        }.to_json)
+
+      expect(form).to be_valid
+    end
+
+    it "is not valid if related item paths do not exist" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: ["/no-such-path"],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/no-such-path"] })
+        .to_return(body: {}.to_json)
+
+      expect(form).to_not be_valid
+    end
+
+    it "is not valid if only some of the paths exist" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: ["/pay-vat", "/no-such-path", "/bank-holidays"],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/pay-vat", "/no-such-path", "/bank-holidays"] })
+        .to_return(body: {
+          "/pay-vat" => "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+          "/bank-holidays" => "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+        }.to_json)
+
+      expect(form).to_not be_valid
+    end
+  end
+
   describe '#links_payload' do
     it 'generates a payload with links' do
       form = TaggingUpdateForm.new(
@@ -9,6 +59,7 @@ RSpec.describe TaggingUpdateForm do
         organisations: [''],
         taxons: [''],
         parent: [''],
+        ordered_related_items: [''],
       )
 
       links_payload = form.links_payload(ContentItemExpandedLinks::TAG_TYPES)
@@ -19,6 +70,7 @@ RSpec.describe TaggingUpdateForm do
         organisations: [],
         taxons: [],
         parent: [],
+        ordered_related_items: [],
       )
     end
 
@@ -37,6 +89,7 @@ RSpec.describe TaggingUpdateForm do
         organisations: [],
         taxons: [],
         parent: [],
+        ordered_related_items: [],
       )
     end
 
@@ -56,6 +109,78 @@ RSpec.describe TaggingUpdateForm do
         organisations: [],
         taxons: [],
         parent: [],
+        ordered_related_items: [],
+      )
+    end
+
+    it "converts base paths of related items into content IDs" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: ['/bank-holidays', '/pay-vat'],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/bank-holidays", "/pay-vat"] })
+        .to_return(body: {
+          "/bank-holidays" => "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "/pay-vat" => "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+        }.to_json)
+
+      links_payload = form.links_payload([:ordered_related_items])
+
+      expect(links_payload).to eql(
+        ordered_related_items: [
+          "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+        ],
+      )
+    end
+
+    it "converts absolute paths of related items into content IDs" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: [
+          'https://www.gov.uk/bank-holidays',
+          'https://www-origin.staging.publishing.service.gov.uk/pay-vat',
+        ],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/bank-holidays", "/pay-vat"] })
+        .to_return(body: {
+          "/bank-holidays" => "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "/pay-vat" => "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+        }.to_json)
+
+      links_payload = form.links_payload([:ordered_related_items])
+
+      expect(links_payload).to eql(
+        ordered_related_items: [
+          "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+        ],
+      )
+    end
+
+    it "preserves the order of related content items" do
+      form = TaggingUpdateForm.new(
+        ordered_related_items: ['/bank-holidays', '/pay-vat', '/additional-state-pension'],
+      )
+
+      stub_request(:post, "https://publishing-api.test.gov.uk/lookup-by-base-path")
+        .with(body: { "base_paths" => ["/bank-holidays", "/pay-vat", "/additional-state-pension"] })
+        .to_return(body: {
+          "/pay-vat" => "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+          "/additional-state-pension" => "e78637eb-3be4-408c-9f9c-d2336635c0ca",
+          "/bank-holidays" => "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+        }.to_json)
+
+      links_payload = form.links_payload([:ordered_related_items])
+
+      expect(links_payload).to eql(
+        ordered_related_items: [
+          "58f79dbd-e57f-4ab2-ae96-96df5767d1b2",
+          "a484eaea-eeb6-48fa-92a7-b67c6cd414f6",
+          "e78637eb-3be4-408c-9f9c-d2336635c0ca",
+        ],
       )
     end
   end
