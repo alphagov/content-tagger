8d7b593d783fcff5316c5ff56e91b0c9b3f02b3b
Tijmen Brommet <tijmen@gmail.com>
2017-01-12 13:26:35 +0000

    Remove slightly redundant contexts
    Less indentation makes it easier to read.

--- spec/services/bulk_tagging/publish_links_spec.rb
+++ spec/services/bulk_tagging/publish_links_spec.rb
@@ -16,80 +16,72 @@ module BulkTagging
     end
 
     describe '.call' do
-      context 'with pre-existing links' do
-        it 'adds the new links to the existing list of links' do
-          publishing_api_has_links(
-            content_id: content_id,
-            links: { taxons: ['existing-content-id'] },
-            version: 10
-          )
+      it 'adds the new links to the existing list of links' do
+        publishing_api_has_links(
+          content_id: content_id,
+          links: { taxons: ['existing-content-id'] },
+          version: 10
+        )
 
-          described_class.new(tag_mapping: tag_mapping).publish
+        described_class.new(tag_mapping: tag_mapping).publish
 
-          expect(Services.publishing_api).to have_received(:patch_links).with(
-            tag_mapping.content_id,
-            links: { 'taxons' => ['existing-content-id', tag_mapping.link_content_id] },
-            previous_version: 10
-          )
-        end
+        expect(Services.publishing_api).to have_received(:patch_links).with(
+          tag_mapping.content_id,
+          links: { 'taxons' => ['existing-content-id', tag_mapping.link_content_id] },
+          previous_version: 10
+        )
       end
 
-      context 'with the same pre-existing links' do
-        it "makes sure we don't duplicate the links" do
-          publishing_api_has_links(
-            content_id: content_id,
-            links: { taxons: [tag_mapping.link_content_id] },
-            version: 10
-          )
+      it "makes sure we don't duplicate the links" do
+        publishing_api_has_links(
+          content_id: content_id,
+          links: { taxons: [tag_mapping.link_content_id] },
+          version: 10
+        )
 
-          described_class.new(tag_mapping: tag_mapping).publish
+        described_class.new(tag_mapping: tag_mapping).publish
 
-          expect(Services.publishing_api).to have_received(:patch_links).with(
-            tag_mapping.content_id,
-            links: { 'taxons' => [tag_mapping.link_content_id] },
-            previous_version: 10
-          )
-        end
+        expect(Services.publishing_api).to have_received(:patch_links).with(
+          tag_mapping.content_id,
+          links: { 'taxons' => [tag_mapping.link_content_id] },
+          previous_version: 10
+        )
       end
 
-      context 'without existing links' do
-        it 'updates the links via the publishing API and marks the tagging as tagged' do
-          publishing_api_has_links(
-            content_id: content_id,
-            links: { taxons: [] },
-            version: 10
-          )
+      it 'adds new links' do
+        publishing_api_has_links(
+          content_id: content_id,
+          links: { taxons: [] },
+          version: 10
+        )
 
-          described_class.new(tag_mapping: tag_mapping).publish
+        described_class.new(tag_mapping: tag_mapping).publish
 
-          expect(Services.publishing_api).to have_received(:patch_links).with(
-            tag_mapping.content_id,
-            links: { tag_mapping.link_type => [tag_mapping.link_content_id] },
-            previous_version: 10
-          )
-        end
+        expect(Services.publishing_api).to have_received(:patch_links).with(
+          tag_mapping.content_id,
+          links: { tag_mapping.link_type => [tag_mapping.link_content_id] },
+          previous_version: 10
+        )
       end
 
-      context 'with the option to delete the source link activated' do
-        it 'adds the new link and remove the old link in the same request' do
-          tagging_source = tag_mapping.tagging_source
-          tagging_source.source_content_id = 'source-content-id'
-          tagging_source.delete_source_link = true
+      it 'adds replaces the link in the same request when if delete source link is chosen' do
+        tagging_source = tag_mapping.tagging_source
+        tagging_source.source_content_id = 'source-content-id'
+        tagging_source.delete_source_link = true
 
-          publishing_api_has_links(
-            content_id: content_id,
-            links: { taxons: ['source-content-id'] },
-            version: 10
-          )
+        publishing_api_has_links(
+          content_id: content_id,
+          links: { taxons: ['source-content-id'] },
+          version: 10
+        )
 
-          described_class.new(tag_mapping: tag_mapping).publish
+        described_class.new(tag_mapping: tag_mapping).publish
 
-          expect(Services.publishing_api).to have_received(:patch_links).with(
-            tag_mapping.content_id,
-            links: { tag_mapping.link_type => [tag_mapping.link_content_id] },
-            previous_version: 10
-          )
-        end
+        expect(Services.publishing_api).to have_received(:patch_links).with(
+          tag_mapping.content_id,
+          links: { tag_mapping.link_type => [tag_mapping.link_content_id] },
+          previous_version: 10
+        )
       end
     end
   end
