a17d5d9ab616cbb0a300a9472203bb32441ca3a6
Carlos Vilhena <carlosvilhena@gmail.com>
2017-01-18 16:31:33 +0000

    Remove unnecessary instance var from controller

--- app/controllers/taxonomies_controller.rb
+++ app/controllers/taxonomies_controller.rb
@@ -1,13 +1,13 @@
 class TaxonomiesController < ApplicationController
   def show
     taxon_content_id = params[:content_id]
-    @taxonomy = Taxonomy::ExpandedTaxonomy.new(taxon_content_id).build
+    taxonomy = Taxonomy::ExpandedTaxonomy.new(taxon_content_id).build
 
     respond_to do |format|
       format.csv do
         send_data(
-          Taxonomy::CsvTreePresenter.new(@taxonomy.child_expansion).present,
-          filename: @taxonomy.root_node.content_item.title + ".csv"
+          Taxonomy::CsvTreePresenter.new(taxonomy.child_expansion).present,
+          filename: taxonomy.root_node.content_item.title + ".csv"
         )
       end
     end
