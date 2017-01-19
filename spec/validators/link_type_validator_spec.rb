4edbdeb2efb5ad157d549c747638c9685afaf45d
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-01-19 10:27:58 +0000

    Correct typo with link_types
    The link_type association is singular, not plural.

--- spec/validators/link_type_validator_spec.rb
+++ spec/validators/link_type_validator_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe LinkTypeValidator do
   it 'validates incorrect link types' do
-    record = double(link_types: ['organisations'], errors: { link_types: [] })
+    record = double(link_type: 'organisations', errors: { link_type: [] })
     described_class.new.validate(record)
 
-    expect(record.errors[:link_types]).to include(/invalid link types found/i)
+    expect(record.errors[:link_type]).to include(/invalid link types found/i)
   end
 
   it 'does not add validation errors when we have expected link types' do
-    record = double(link_types: ['taxons'], errors: {})
+    record = double(link_type: 'taxons', errors: {})
     described_class.new.validate(record)
 
     expect(record.errors).to be_empty
