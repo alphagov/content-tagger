dc52496485c9987aa83e9fe5ffe610b19cfcb4a0
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2017-01-25 15:39:33 +0000

    Remove unused base path prefixes
    Now that we've renamed all the education taxons, and deleted the
    rest, we don't need this code any more.

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,11 +1,7 @@
 class Theme
-  # TODO: remove these once no longer used in base paths
-  ALPHA_TAXONOMY = '/alpha-taxonomy'.freeze
-  OLD_EDUCATION_THEME_BASE_PATH = '/education-training-and-skills'.freeze
-
   EDUCATION_THEME_BASE_PATH = '/education'.freeze
 
   def self.taxon_path_prefixes
-    [EDUCATION_THEME_BASE_PATH, OLD_EDUCATION_THEME_BASE_PATH, ALPHA_TAXONOMY]
+    [EDUCATION_THEME_BASE_PATH]
   end
 end
