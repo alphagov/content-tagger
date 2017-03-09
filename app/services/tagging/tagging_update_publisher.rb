ce630b38802426c87a239ac34b7f0f640c2dfbab
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2017-03-09 11:08:02 +0000

    Test that links are valid against link schema
    - Added in a test to check that the links payload is valid against
    the schemas in govuk_content_schemas.

--- app/services/tagging/tagging_update_publisher.rb
+++ app/services/tagging/tagging_update_publisher.rb
@@ -22,7 +22,25 @@ module Tagging
       )
     end
 
+    def generate_links_payload
+      content_item.allowed_tag_types.reduce({}) do |payload, tag_type|
+        content_ids = fetch_content_ids(tag_type)
+
+        payload.merge(tag_type => content_ids)
+      end
+    end
+
   private
+
+    def fetch_content_ids(tag_type)
+      if tag_type == :ordered_related_items
+        related_content_items.map(&:content_id)
+      elsif tag_type == :ordered_related_items_overrides
+        related_content_items_overrides.map(&:content_id)
+      else
+        clean_input_array(params[tag_type])
+      end
+    end
 
     def valid?
       related_content_items.each do |related_item|
@@ -36,20 +54,6 @@ module Tagging
       end
 
       @related_item_errors.none? && @related_item_overrides_errors.none?
-    end
-
-    def generate_links_payload
-      content_item.allowed_tag_types.reduce({}) do |payload, tag_type|
-        content_ids = if tag_type == :ordered_related_items
-                        related_content_items.map(&:content_id)
-                      elsif tag_type == :ordered_related_items_overrides
-                        related_content_items_overrides.map(&:content_id)
-                      else
-                        clean_input_array(params[tag_type])
-                      end
-
-        payload.merge(tag_type => content_ids)
-      end
     end
 
     def related_content_items
