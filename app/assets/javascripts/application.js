cb2a7c14467f8e0986df0349adf8807024dbd0b3
Tijmen Brommet <tijmen@gmail.com>
2017-07-21 08:16:49 +0100

    Move datatables.js to /vendor
    We shouldn't lint external libraries.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 //= require_tree .
 //= require select2
 //= require js.cookies.js
+//= require datatables.min.js
 //= require_self
 //= require jquery-ui
 //= require Chart.bundle
 //= require chartkick
-//= require vendor/datatables.min
 
 $(document).ready(function() {
   $(".select2").select2({ allowClear: true });
