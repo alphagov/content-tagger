fa95cdf9b85e67f67bfd8f9faff88156bcc5b973
Tom Gladhill <whoojemaflip@gmail.com>
2017-08-23 11:32:06 +0100

    Fixes a race-hazard condition causing errors on project create
    Jobs were being enqueued and running before the transaction completed.

--- app/lib/project_builder.rb
+++ app/lib/project_builder.rb
@@ -1,16 +1,19 @@
 class ProjectBuilder
   def self.call(project_name, taxonomy_branch_content_id, content_item_attributes_enum)
-    ProjectContentItem.transaction do
+    project = ProjectContentItem.transaction do
       Project
         .create!(name: project_name, taxonomy_branch: taxonomy_branch_content_id)
         .tap do |project|
           content_item_attributes_enum.each do |content_item_attributes|
-            content_item = ProjectContentItem.create!(
+            ProjectContentItem.create!(
               { project: project }.merge(content_item_attributes)
             )
-            LookupContentIdWorker.perform_async(content_item.id)
           end
         end
+    end
+
+    project.content_items.each do |content_item|
+      LookupContentIdWorker.perform_async(content_item.id)
     end
   end
 end
