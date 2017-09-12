330d4718c84d6856b6c982b406fea641a036f814
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2017-09-12 16:32:40 +0100

    Add filtering on name to tagging actvity

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,6 +2,11 @@ class AnalyticsController < ApplicationController
   before_action :ensure_user_can_access_tagathon_tools!
 
   def show
-    render :show, locals: { page: Analytics::IndexPage.new }
+    render :show, locals: { page: Analytics::IndexPage.new({link_types: ['taxons']}.merge(filter_params))}
   end
+
+  def filter_params
+    params.permit(users: []).to_h.symbolize_keys
+  end
+
 end
--- app/services/analytics/index_page.rb
+++ app/services/analytics/index_page.rb
@@ -3,26 +3,31 @@ module Analytics
 
     attr_reader :link_changes
 
-    def initialize(params = {})
-      @params = params
-      @link_changes = get_link_changes
+    def initialize(params = {link_types: ['taxons']})
+      @link_changes = get_link_changes(params)
+    end
+
+    def users
+      @link_changes.map {|link_change| link_change.slice(:user_uid, :user_name)}.compact.uniq
     end
 
   private
 
-    def get_link_changes
+    def get_link_changes(params)
       results = Services.link_changes_api
-                  .get_link_changes(link_types: ['taxons'])
+                  .get_link_changes(params)
                   .to_hash
                   .deep_symbolize_keys[:link_changes]
 
       results.each do |result|
         user = User.find_by_uid(result[:user_uid])
         if user.present?
+          result[:user_uid] = user.uid
           result[:user_name] = user.name
           result[:organisation] = user.organisation_slug.capitalize.gsub('-', ' ')
         end
       end
+
       results
 
     end
