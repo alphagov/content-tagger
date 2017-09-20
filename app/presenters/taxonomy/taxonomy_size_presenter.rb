cb62648703db5eed1373de57a8ceb1e56c5619f1
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-09-20 09:42:46 +0100

    Handle taxonomy branches with no content in TaxonomySizePresenter
    Return 0 for bar_width_percentage if max_size is 0.

--- app/presenters/taxonomy/taxonomy_size_presenter.rb
+++ app/presenters/taxonomy/taxonomy_size_presenter.rb
@@ -3,6 +3,7 @@ module Taxonomy
     MAXIMUM_BAR_WIDTH_PERCENTAGE = 30
 
     def bar_width_percentage(size)
+      return 0 if max_size.zero?
       (size * MAXIMUM_BAR_WIDTH_PERCENTAGE) / max_size
     end
   end
