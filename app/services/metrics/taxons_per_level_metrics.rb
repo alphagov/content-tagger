431849214db53c41db311e18a667b27d3357b56b
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-01-11 16:18:33 +0000

    Add statsd metrics for number of taxons per level

---
+++ app/services/metrics/taxons_per_level_metrics.rb
@@ -0,0 +1,17 @@
+require_relative '../metrics'
+
+module Metrics
+  class TaxonsPerLevelMetrics
+    def count_taxons_per_level
+      Taxonomy::TaxonomyQuery.new.taxons_per_level.to_enum.with_index(1).each do |taxons, level|
+        gauge("level_#{level}.number_of_taxons", taxons.length)
+      end
+    end
+
+  private
+
+    def gauge(stat, value)
+      Metrics.statsd.gauge(stat, value)
+    end
+  end
+end
