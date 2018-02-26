818cfc3165ac82cc37b66e562fa8cefe6f9f209e
Tom Sabin <tom.sabin@digital.cabinet-office.gov.uk>
2018-02-26 10:37:50 +0000

    Add required fields for PUT content payload
    Use Taxonomy::BuildTaxonPayload to collect the correct required fields
    for the request.

--- app/services/taxonomy/bulk_update_taxon.rb
+++ app/services/taxonomy/bulk_update_taxon.rb
@@ -7,7 +7,10 @@ module Taxonomy
 
     def bulk_update
       nested_tree.each do |taxon|
-        UpdateTaxonWorker.perform_async(taxon.content_id, update_payload(taxon))
+        UpdateTaxonWorker.perform_async(
+          taxon.content_id,
+          @attributes.slice(:phase)
+        )
       end
     end
 
@@ -19,16 +22,6 @@ module Taxonomy
           content_id: @root_taxon_content_id,
           publishing_api: Services.publishing_api
         )
-    end
-
-    def update_payload(taxon)
-      {
-        document_type: 'taxon',
-        publishing_app: 'content-tagger',
-        schema_name: 'taxon',
-        title: taxon.title,
-        phase: @attributes.fetch(:phase),
-      }
     end
   end
 end
--- app/workers/update_taxon_worker.rb
+++ app/workers/update_taxon_worker.rb
@@ -1,12 +1,14 @@
 class UpdateTaxonWorker
   include Sidekiq::Worker
 
-  def perform(content_id, payload)
-    taxon = Services.publishing_api.get_content(content_id)
+  def perform(content_id, attributes)
+    taxon = Taxonomy::BuildTaxon.call(content_id: content_id)
+    taxon.phase = attributes.with_indifferent_access.fetch(:phase)
 
+    payload = Taxonomy::BuildTaxonPayload.call(taxon: taxon)
     Services.publishing_api.put_content(content_id, payload)
 
-    return unless taxon.to_h['publication_state'] == 'published'
+    return unless taxon.published?
 
     Services.publishing_api.publish(content_id)
   end
