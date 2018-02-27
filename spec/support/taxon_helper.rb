c828e992815b3cd1558de8d5549b228b502ef5c8
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-02-27 16:31:05 +0000

    Add helper to test using expanded links

---
+++ spec/support/taxon_helper.rb
@@ -0,0 +1,33 @@
+module Support
+  class TaxonHelper
+    def self.expanded_link_hash(content_id, paths)
+      path_converter = lambda do |path|
+        head, *tail = path
+
+        return {} if head.nil?
+        links_hash =
+          if tail.empty?
+            {}
+          else
+            {
+              "links" =>
+               {
+                 tail.length == 1 ? "root_taxon" : "parent_taxons" => [path_converter.call(tail)]
+               }
+            }
+          end
+        {
+          "content_id" => head
+        }.merge(links_hash)
+      end
+
+      {
+        'content_id' => content_id,
+        'expanded_links' =>
+          {
+            'taxons' => paths.map { |path| path_converter.call(path.reverse) }
+          }
+      }
+    end
+  end
+end
