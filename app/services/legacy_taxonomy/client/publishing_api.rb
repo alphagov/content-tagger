a18ba0ee98fcc6357c1587446efa20a125de81fa
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-03-27 11:53:31 +0100

    Add helper methods to facilitate access to the publishing api
    all_taxons_content_ids: gets all new taxons
    legacy_content_ids: gets all legacy taxons for a given new taxon
    content_ids_linked_to_taxon: Gets all content tagged to a new taxon.

--- app/services/legacy_taxonomy/client/publishing_api.rb
+++ app/services/legacy_taxonomy/client/publishing_api.rb
@@ -32,6 +32,30 @@ module LegacyTaxonomy
           response = Services.publishing_api.get_content(content_id)
           response.dig('details', 'groups') || []
         end
+
+        def all_taxons_content_ids
+          enum = Services.publishing_api.get_content_items_enum(document_type: 'taxon', fields: ['content_id'], states: ['published']).lazy
+          enum.map { |content_item| content_item['content_id'] }
+        end
+
+        def legacy_content_ids(taxon_content_id)
+          hash = Services.publishing_api.get_expanded_links(taxon_content_id).to_h
+          legacy_taxons = hash.dig('expanded_links', 'legacy_taxons') || []
+          legacy_taxons.map { |taxon| taxon['content_id'] }
+        rescue GdsApi::HTTPGatewayTimeout
+          retries ||= 0
+          retry if (retries += 1) < 3
+          raise
+        end
+
+        def content_ids_linked_to_taxon(taxon_content_id)
+          content = Services.publishing_api.get_linked_items(taxon_content_id, link_type: 'taxons', fields: ['content_id'])
+          content.map { |taxon| taxon['content_id'] }
+        rescue GdsApi::HTTPGatewayTimeout
+          retries ||= 0
+          retry if (retries += 1) < 3
+          raise
+        end
       end
     end
   end
