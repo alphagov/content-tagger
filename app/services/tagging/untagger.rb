921b8026aca55d0708021f2fcf61e4d9be58b407
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-04-05 11:54:56 +0100

    Use version number in untagger

--- app/services/tagging/untagger.rb
+++ app/services/tagging/untagger.rb
@@ -5,8 +5,17 @@ module Tagging
     end
 
     def untag(content_id, taxon_content_ids)
-      existing_taxons_ids = Services.publishing_api.get_links(content_id).dig('links', 'taxons')
-      Services.publishing_api.patch_links(content_id, links: { taxons: (existing_taxons_ids - taxon_content_ids) })
+      response = Services.publishing_api.get_links(content_id)
+      existing_taxons_ids = response.dig('links', 'taxons')
+      version = response['version']
+
+      Services.publishing_api.patch_links(content_id,
+                                          previous_version: version,
+                                          links: { taxons: (existing_taxons_ids - taxon_content_ids) })
+    rescue GdsApi::HTTPConflict, GdsApi::HTTPGatewayTimeout, GdsApi::TimedOutException
+      retries ||= 0
+      retry if (retries += 1) < 3
+      raise
     end
   end
 end
