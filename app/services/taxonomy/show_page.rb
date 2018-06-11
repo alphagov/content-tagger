73acd69e0b037705fd31744b90436afba54747cc
Michael Walker <mike@barrucadu.co.uk>
2018-06-11 15:52:32 +0100

    Display 0 email subscriptions if the list does not exist

--- app/services/taxonomy/show_page.rb
+++ app/services/taxonomy/show_page.rb
@@ -73,6 +73,8 @@ module Taxonomy
         begin
           email_lists = Services.email_alert_api.find_subscriber_list("links" => { taxon_tree: [taxon.content_id] })
           email_lists.dig("subscriber_list", "active_subscriptions_count")
+        rescue GdsApi::HTTPNotFound
+          "0"
         rescue GdsApi::BaseError, SocketError => e
           GovukError.notify(e)
           "?"
