b52f023ac473a9e2a9ed4d953dd81ddfeba44230
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-06-13 13:49:18 +0100

    Reformat the ChildTaxonCountMetric class, for readability
    Break up some of the longer lines and strings.

--- app/workers/taxonomy_health/child_taxon_count_metric.rb
+++ app/workers/taxonomy_health/child_taxon_count_metric.rb
@@ -6,16 +6,29 @@ module TaxonomyHealth
     def perform(arguments)
       maximum = arguments.symbolize_keys[:maximum]
       minimum = arguments.symbolize_keys[:minimum] || 0
-      taxonomy = Taxonomy::ExpandedTaxonomy.new(GovukTaxonomy::ROOT_CONTENT_ID).build.child_expansion
+
+      taxonomy = Taxonomy::ExpandedTaxonomy
+                   .new(GovukTaxonomy::ROOT_CONTENT_ID)
+                   .build.child_expansion
+
       taxon_counts = taxonomy.tree.map do |linked_content_item|
-        { linked_content_item: linked_content_item, count: linked_content_item.children.count }
+        {
+          linked_content_item: linked_content_item,
+          count: linked_content_item.children.count
+        }
       end
+
       taxon_counts.select { |item| item[:count] > maximum }.each do |item|
-        message = "Taxon has #{pluralize(item[:count], 'child')}, which exceeds the maximum of #{maximum}"
+        message = "Taxon has #{pluralize(item[:count], 'child')}, "\
+                  "which exceeds the maximum of #{maximum}"
+
         create_health_warning(item[:linked_content_item], message)
       end
+
       taxon_counts.select { |item| item[:count].positive? && item[:count] < minimum }.each do |item|
-        message = "Taxon has #{pluralize(item[:count], 'child')}, which is fewer than the minimum of #{minimum}"
+        message = "Taxon has #{pluralize(item[:count], 'child')}, "\
+                  "which is fewer than the minimum of #{minimum}"
+
         create_health_warning(item[:linked_content_item], message)
       end
     end
@@ -23,12 +36,14 @@ module TaxonomyHealth
   private
 
     def create_health_warning(linked_content_item, message)
-      Taxonomy::HealthWarning.create(content_id: linked_content_item.content_id,
-                                     title: linked_content_item.title,
-                                     internal_name: linked_content_item.internal_name,
-                                     path: linked_content_item.base_path,
-                                     metric: self.class.to_s,
-                                     message: message)
+      Taxonomy::HealthWarning.create(
+        content_id: linked_content_item.content_id,
+        title: linked_content_item.title,
+        internal_name: linked_content_item.internal_name,
+        path: linked_content_item.base_path,
+        metric: self.class.to_s,
+        message: message
+      )
     end
   end
 end
