ab1927da970082ab7409c58047ceeb38605e34aa
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-06-13 14:07:27 +0100

    Neaten up the line length in the MaximumDepthMetric class
    And also rename children_with_depth to children_with_depth_exceeding
    to try and indicate that this is a filtering operation.

--- app/workers/taxonomy_health/maximum_depth_metric.rb
+++ app/workers/taxonomy_health/maximum_depth_metric.rb
@@ -4,23 +4,32 @@ module TaxonomyHealth
 
     def perform(arguments)
       maximum_depth = arguments.symbolize_keys[:maximum_depth]
-      taxonomy = Taxonomy::ExpandedTaxonomy.new(GovukTaxonomy::ROOT_CONTENT_ID).build.child_expansion
-      children_with_depth(taxonomy, maximum_depth).each do |linked_content_item|
-        Taxonomy::HealthWarning.create(content_id: linked_content_item.content_id,
-                                       title: linked_content_item.title,
-                                       internal_name: linked_content_item.internal_name,
-                                       path: linked_content_item.base_path,
-                                       metric: self.class.to_s,
-                                       message: "Taxon exceeds depth of #{maximum_depth}")
+
+      taxonomy = Taxonomy::ExpandedTaxonomy
+                   .new(GovukTaxonomy::ROOT_CONTENT_ID)
+                   .build.child_expansion
+
+      children_with_depth_exceeding(
+        taxonomy,
+        maximum_depth
+      ).each do |linked_content_item|
+        Taxonomy::HealthWarning.create(
+          content_id: linked_content_item.content_id,
+          title: linked_content_item.title,
+          internal_name: linked_content_item.internal_name,
+          path: linked_content_item.base_path,
+          metric: self.class.to_s,
+          message: "Taxon exceeds depth of #{maximum_depth}"
+        )
       end
     end
 
   private
 
-    def children_with_depth(linked_content_item, maximum_depth, current_depth = 0)
+    def children_with_depth_exceeding(linked_content_item, maximum_depth, current_depth = 0)
       result = current_depth > maximum_depth ? [linked_content_item] : []
       result + linked_content_item.children.flat_map do |child|
-        children_with_depth(child, maximum_depth, current_depth + 1)
+        children_with_depth_exceeding(child, maximum_depth, current_depth + 1)
       end
     end
   end
