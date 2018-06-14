77cf1019adc6cdd32c468305028a44774c0f2f2a
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-06-14 13:14:53 +0100

    Improve the line length in child_taxon_count_metric_spec.rb

--- spec/workers/taxonomy_health/child_taxon_count_metric_spec.rb
+++ spec/workers/taxonomy_health/child_taxon_count_metric_spec.rb
@@ -1,14 +1,22 @@
 require "rails_helper"
 
 RSpec.describe TaxonomyHealth::ChildTaxonCountMetric do
-  let(:home_page) { FactoryBot.build(:taxon_hash, :home_page, expanded_links: { level_one_taxons: [food] }) }
+  let(:home_page) do
+    FactoryBot.build(
+      :taxon_hash,
+      :home_page,
+      expanded_links: { level_one_taxons: [food] }
+    )
+  end
   let(:food) do
-    FactoryBot.build(:taxon_hash,
-                     title: 'Food',
-                     expanded_links: {
-                       child_taxons: [fruits, vegetables, meats],
-                       root_taxon: [GovukTaxonomy::ROOT_CONTENT_ID]
-                     })
+    FactoryBot.build(
+      :taxon_hash,
+      title: 'Food',
+      expanded_links: {
+        child_taxons: [fruits, vegetables, meats],
+        root_taxon: [GovukTaxonomy::ROOT_CONTENT_ID]
+      }
+    )
   end
   let(:fruits) { FactoryBot.build(:taxon_hash, title: 'Fruits') }
   let(:vegetables) { FactoryBot.build(:taxon_hash, title: 'Vegetables') }
@@ -26,16 +34,21 @@ RSpec.describe TaxonomyHealth::ChildTaxonCountMetric do
   end
 
   it 'records no failing taxons' do
-    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 3, minimum: 1) }.to_not(change { Taxonomy::HealthWarning.count })
+    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 3, minimum: 1) }
+      .to_not(change { Taxonomy::HealthWarning.count })
   end
 
   it 'records a failing taxon with too many children' do
-    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 2) }.to(change { Taxonomy::HealthWarning.count }.by(1))
+    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 2) }
+      .to(change { Taxonomy::HealthWarning.count }.by(1))
+
     expect(Taxonomy::HealthWarning.last.content_id).to eq(food['content_id'])
   end
 
   it 'records a failing taxon with too few children - excluding leaf nodes' do
-    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 3, minimum: 2) }.to(change { Taxonomy::HealthWarning.count }.by(1))
+    expect { TaxonomyHealth::ChildTaxonCountMetric.new.perform(maximum: 3, minimum: 2) }
+      .to(change { Taxonomy::HealthWarning.count }.by(1))
+
     expect(Taxonomy::HealthWarning.last.content_id).to eq(home_page['content_id'])
   end
 end
