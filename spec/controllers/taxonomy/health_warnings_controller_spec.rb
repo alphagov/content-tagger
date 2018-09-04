2f273178e4fd5e81233106f57977c4a7dd288280
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2018-09-04 09:10:28 +0000

    Fix Response#success? deprecation warning

--- spec/controllers/taxonomy/health_warnings_controller_spec.rb
+++ spec/controllers/taxonomy/health_warnings_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Taxonomy::HealthWarningsController, type: :controller do
   describe "GET #index" do
     it "returns a success response" do
       get :index, params: {}
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 end
