6776ff9efb818ed1602d18ffefa4fe76812ec473
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2019-03-21 17:28:40 +0000

    Remove unused code

--- app/services/facets/tagging_update_form.rb
+++ app/services/facets/tagging_update_form.rb
@@ -16,14 +16,6 @@ module Facets
 
       tag_values = TAG_TYPES.each_with_object({}) do |tag_type, current_tags|
         current_tags[tag_type] = links.send(tag_type).map { |links_hash| links_hash["content_id"] }
-
-        base_paths = links.send(tag_type).map { |links_hash| links_hash["base_path"] }
-
-        # The number of extra empty form fields to add to a link section when the link
-        # section shows an individual form input for each value. This allows users to
-        # append new links to the end of the existing list.
-        empty_entries = [""] * 5
-        current_tags[tag_type] = base_paths + empty_entries
       end
 
       new(
