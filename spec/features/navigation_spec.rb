3d584e95299d693f97181734488a14872ebac34f
Aga Dufrat <agnieszka.dufrat@digital.cabinet-office.gov.uk>
2019-04-12 13:45:21 +0100

    Update navigation spec
    to include "Tagging history" and "Taxonomy Health" navigation options in the
    permission tests. Add a test to check if clicking the link redirects to the
    correct page.

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -28,6 +28,24 @@ RSpec.feature "Navigation", type: :feature do
     then_i_can_see_the_full_set_of_navigation_options
   end
 
+  context "Clicking navigation option" do
+    before do
+      given_i_am_logged_in_as_a_gds_editor
+      when_i_visit_the_application
+    end
+
+    navigation_options = ["Edit a page", "Bulk tag", "Edit taxonomy", "Tagging history", "Taxonomy Health", "Projects", "Facets"].freeze
+    navigation_paths = %w[/taggings/lookup /bulk_tag/new /taxons /tagging_history /taxonomy/health_warnings /projects /facet_groups].freeze
+
+    navigation_options.zip(navigation_paths).each do |option, path|
+      scenario "#{option} redirects to the correct page" do
+        stub_get_request(option)
+        and_when_i_click_navigation_option(option)
+        then_i_am_redirected_to_the_page(path)
+      end
+    end
+  end
+
   def given_i_am_logged_in_as_a_user_with_no_special_permissions
     login_as create(:user)
   end
@@ -54,6 +72,8 @@ RSpec.feature "Navigation", type: :feature do
       expect(page).not_to have_text "Edit a page"
       expect(page).not_to have_text "Bulk tag"
       expect(page).not_to have_text "Edit taxonomy"
+      expect(page).not_to have_text "Tagging history"
+      expect(page).not_to have_text "Taxonomy Health"
       expect(page).not_to have_text "Projects"
       expect(page).not_to have_text "Facets"
     end
@@ -64,6 +84,8 @@ RSpec.feature "Navigation", type: :feature do
       expect(page).not_to have_text "Edit a page"
       expect(page).not_to have_text "Bulk tag"
       expect(page).not_to have_text "Edit taxonomy"
+      expect(page).not_to have_text "Tagging history"
+      expect(page).not_to have_text "Taxonomy Health"
       expect(page).not_to have_text "Facets"
       expect(page).to have_text "Projects"
     end
@@ -74,8 +96,35 @@ RSpec.feature "Navigation", type: :feature do
       expect(page).to have_text "Edit a page"
       expect(page).to have_text "Bulk tag"
       expect(page).to have_text "Edit taxonomy"
+      expect(page).to have_text "Tagging history"
+      expect(page).to have_text "Taxonomy Health"
       expect(page).to have_text "Projects"
       expect(page).to have_text "Facets"
     end
+  end
+
+  def and_when_i_click_navigation_option(option)
+    within "nav .navbar-nav" do
+      click_link option
+    end
+  end
+
+  def then_i_am_redirected_to_the_page(path)
+    expect(page).to have_current_path(path)
+  end
+
+  def stub_get_request(option)
+    stubbed_request_data = {
+      "Facets" => {
+        url: "https://publishing-api.test.gov.uk/v2/content?document_type=facet_group&order=-public_updated_at&page=1&per_page=50&q=&search_in%5B%5D=title&states%5B%5D=published",
+        body: { results: [] }
+      },
+      "Tagging history" => {
+        url: "https://publishing-api.test.gov.uk/v2/links/changes?link_types%5B%5D=taxons",
+        body: { link_changes: [] }
+      }
+    }
+
+    stub_request(:get, stubbed_request_data[option][:url]).to_return(status: 200, body: stubbed_request_data[option][:body].to_json) unless stubbed_request_data[option].nil?
   end
 end
