a43a1ae1564c4976fe3a078ad60eda114ae11e50
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2019-07-15 11:40:24 +0100

    Adds count of documents tagged to each branch of the taxonomy to the final row of the CSV

--- app/presenters/taxonomy/csv_tree_presenter.rb
+++ app/presenters/taxonomy/csv_tree_presenter.rb
@@ -11,7 +11,27 @@ module Taxonomy
         @tree.each do |node|
           row = [node.title]
           node.depth.times { row.unshift nil }
+          row << get_count(node.content_id)
           csv << row
+        end
+      end
+    end
+
+  private
+
+    def get_count(content_id)
+      taxonomy_tree_counts[content_id]
+    end
+
+    def taxonomy_tree_counts
+      @taxonomy_tree_counts ||= begin
+        params = {
+          count: 0,
+          facet_taxons: 4000
+        }
+        results = Services.search_api.search(params).to_hash
+        results['facets']['taxons']['options'].each_with_object({}) do |option, taxonomy_tree_counts|
+          taxonomy_tree_counts[option['value']['slug']] = option['documents']
         end
       end
     end
