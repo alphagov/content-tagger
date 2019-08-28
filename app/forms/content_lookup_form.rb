e99c531b492afe216beb4b89513bfaae0d92757e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-08-28 16:58:08 +0100

    Allow draft content to be tagged
    We allow other publishing apps like Whitehall to tag draft content, so
    it should be allowed here as well.

--- app/forms/content_lookup_form.rb
+++ app/forms/content_lookup_form.rb
@@ -8,7 +8,7 @@ class ContentLookupForm
   def content_id
     @content_id ||= begin
       Services.statsd.time "base_path_lookup" do
-        Services.publishing_api.lookup_content_id(base_path: base_path)
+        Services.publishing_api.lookup_content_id(base_path: base_path, with_drafts: true)
       end
     end
   end
