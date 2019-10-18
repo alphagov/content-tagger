539ad001fdc10c85a3b70b8c01044a3ab633e5ed
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-10-18 16:06:13 +0100

    Publish Taxon Worker publishes Brexit taxon with 'cy' locale

--- app/workers/publish_taxon_worker.rb
+++ app/workers/publish_taxon_worker.rb
@@ -1,8 +1,13 @@
 class PublishTaxonWorker
   include Sidekiq::Worker
+  BREXIT_TAXON_CONTENT_ID = "d6c2de5d-ef90-45d1-82d4-5f2438369eea".freeze
 
   def perform(taxon_content_id)
     Services.publishing_api.publish(taxon_content_id)
+
+    if taxon_content_id == BREXIT_TAXON_CONTENT_ID
+      Services.publishing_api.publish(taxon_content_id, nil, locale: "cy")
+    end
   rescue GdsApi::HTTPConflict => e # Ignore attempts to publish already published content
     puts "409 #{e.message}"
   end
