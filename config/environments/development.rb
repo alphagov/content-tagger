fb9489744f54a9dcc828f9970907cde5db00c38c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-06 11:28:17 +0000

    Relax authorized host domains in development
    https://github.com/alphagov/govuk-docker/issues/176
    
    This changes the development configuration to allow requests from
    any domain. While this will include *.dev.gov.uk, this reduces the
    coupling to that specific domain without any extra effort, thus
    supporting more use cases like Docker training.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   # Raises error for missing translations.
   config.action_view.raise_on_missing_translations = true
 
+  # Allow requests for all domains e.g. <app>.dev.gov.uk
+  config.hosts.clear
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
