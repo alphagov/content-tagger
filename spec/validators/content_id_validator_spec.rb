62fa9b8b33c2f414a29ae309aad9889b4182e141
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-02-02 13:12:38 +0000

    Fix deprecation warnings

--- spec/validators/content_id_validator_spec.rb
+++ spec/validators/content_id_validator_spec.rb
@@ -1,15 +1,19 @@
 require "rails_helper"
 
 RSpec.describe ContentIdValidator do
+  let(:record) { build_stubbed(:tag_mapping) }
+
   it "validates a missing content id" do
-    record = double(content_id: nil, errors: { content_id: [] })
+    allow(record).to receive(:content_id).and_return nil
+
     described_class.new.validate(record)
 
     expect(record.errors[:content_id]).to include(/we could not find this url/i)
   end
 
   it "does not add validation errors when content id exists" do
-    record = double(content_id: "a-content-ID", errors: {})
+    allow(record).to receive(:content_id).and_return "a-content-ID"
+
     described_class.new.validate(record)
 
     expect(record.errors).to be_empty
