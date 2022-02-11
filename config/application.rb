3da34d0bd7f289f7401f42397b4abc465ad1ecff
Ollie Treend <ollie.treend@digital.cabinet-office.gov.uk>
2022-02-11 13:47:37 +0000

    Add cookie rotator to upgrade SHA1 sessions to SHA256

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,22 @@ module ContentTagger
     config.blacklisted_tag_types = config_for(:blacklisted_tag_types)
 
     config.active_record.belongs_to_required_by_default = false
+
+    # Rotate SHA1 cookies to SHA256 (the new Rails 7 default)
+    # TODO: Remove this after existing user sessions have been rotated
+    # https://guides.rubyonrails.org/v7.0/upgrading_ruby_on_rails.html#key-generator-digest-class-changing-to-use-sha256
+    Rails.application.config.action_dispatch.cookies_rotations.tap do |cookies|
+      salt = Rails.application.config.action_dispatch.authenticated_encrypted_cookie_salt
+      secret_key_base = Rails.application.secrets.secret_key_base
+      next if secret_key_base.blank?
+
+      key_generator = ActiveSupport::KeyGenerator.new(
+        secret_key_base, iterations: 1000, hash_digest_class: OpenSSL::Digest::SHA1
+      )
+      key_len = ActiveSupport::MessageEncryptor.key_len
+      secret = key_generator.generate_key(salt, key_len)
+
+      cookies.rotate :encrypted, secret
+    end
   end
 end
