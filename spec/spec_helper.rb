33032feab51ae86ef474dfe8edd511acf1c7ded9
Kevin Dew <kevindew@me.com>
2022-10-17 14:14:56 +0100

    Remove unnecessary fixtures config
    We don't make use of fixtures so don't need to configure them.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,6 @@ RSpec.configure do |config|
   config.order = :random
   Kernel.srand config.seed
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
