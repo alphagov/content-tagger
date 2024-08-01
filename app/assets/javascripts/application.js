df5f739251d5c732f1ae29a97d105494ed32be4e
Mark Taylor <138604938+mtaylorgds@users.noreply.github.com>
2024-08-01 15:22:45 +0100

    Fix Javascript error on page load
    With the update to the publishing components gem the call to
     `window.GOVUK.approveAllCookieTypes()` in
     `app/assets/javascripts/application.js` is failing. Move the call
     inside a `$(document).ready` handler and add a require for the cookie
     functions code.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,10 +8,11 @@
 //= require jquery.waypoints.min.js
 
 //= require govuk_publishing_components/dependencies
+//= require govuk_publishing_components/lib/cookie-functions
 
 $(document).ready(function () {
   $('.select2:not(.tagging_project):not(.bulk_tagger)').select2({ allowClear: true })
-})
 
-window.GOVUK.approveAllCookieTypes()
-window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
+  window.GOVUK.approveAllCookieTypes()
+  window.GOVUK.cookie('cookies_preferences_set', 'true', { days: 365 })
+})
