68e7835ad637f164de9c2ef30ffa00e5946423ae
Barbara <barbara.slawinska@digital.cabinet-office.gov.uk>
2025-12-22 14:06:07 +0000

    Upgrade Ruby to 3.4.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2018_06_14_154832) do
+ActiveRecord::Schema[8.0].define(version: 2018_06_14_154832) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "project_content_items", id: :serial, force: :cascade do |t|
     t.string "url"
